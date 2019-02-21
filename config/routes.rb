c8cd111c32d14f0b490671fcbc72e9592a7dde55
Tyler Fisher <tfisher21@gmail.com>
2019-02-20 19:06:25 -0600

    Create index and show routes and controllers for the organization model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/people.json"      =>  "people#index"
-  get "/people/:id.json"  =>  "people#show"
+  get "/people.json"            =>  "people#index"
+  get "/people/:id.json"        =>  "people#show"
+
+  get "/organizations.json"     =>  "organizations#index"
+  get "/organizations/:id.json" =>  "organizations#show"
 end
