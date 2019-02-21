daca268b5179c92842f574013ce90b94470c0a95
Tyler Fisher <tfisher21@gmail.com>
2019-02-20 21:42:20 -0600

    Migrate db from sqlite to postgres in preparation for Heroku migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_20_022406) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "addresses", force: :cascade do |t|
     t.string "street"
     t.string "city"
