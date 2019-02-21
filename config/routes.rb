229900f07f87b367655b0185fd1e5db00d15aabf
Tyler Fisher <tfisher21@gmail.com>
2019-02-20 21:51:58 -0600

    Add root redirect to people#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/', to: redirect('/people.json')
+
   get "/people.json"            =>  "people#index"
   get "/people/:id.json"        =>  "people#show"
 
